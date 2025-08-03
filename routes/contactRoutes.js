f9238c7461ea2cdc3945813cb40da479f99cac72
B S Ovhalkar <bhavaneshsovhalkar@gmail.com>
2025-08-03 10:34:23 +0530

    Added All Routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -9,5 +9,25 @@ router.route('/').get((req,res)=>{
         {message : "get all contacts"}
     );
 });
+router.route('/').post((req,res)=>{
+    res.status(200).json(
+        {message : "Create contant"}
+    );
+});
+router.route('/:id').get((req,res)=>{
+    res.status(200).json(
+        {message : `get contact with id ${req.params.id}`}
+    );
+});
+router.route('/:id').put((req,res)=>{
+    res.status(200).json(
+        {message : `edit contact with id ${req.params.id}`}
+    );
+});
+router.route('/:id').delete((req,res)=>{
+    res.status(200).json(
+        {message : `Delete contact with id ${req.params.id}`}
+    );
+});
 
 module.exports = router;
