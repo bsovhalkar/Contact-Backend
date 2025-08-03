677a40958d6f04921c354e1f2ec0d716c897ecd6
B S Ovhalkar <bhavaneshsovhalkar@gmail.com>
2025-08-03 10:26:53 +0530

    Added first route

---
+++ routes/contactRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+
+const router = express.Router();
+
+
+router.route('/').get((req,res)=>{
+    res.status(200).json(
+        {message : "get all contacts"}
+    );
+});
+
+module.exports = router;
